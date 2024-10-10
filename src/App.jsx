cead93e183e886c49abcb24efa38013f29453962
rintarotajima <rintarotajima@gmail.com>
2024-10-10 14:01:05 +0900

    react-router-domのバージョンを6から5に変更

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Page1 } from "./Page1";
 import { Page1DetailA } from "./Page1DetailA";
 import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route} from "react-router-dom";
 
 export default function App() {
   return (
@@ -13,14 +13,18 @@ export default function App() {
       <Link to="/page1">Page1</Link>
       <br />
       <Link to="/page2">Page2</Link>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />}>
-          <Route path="detailA" element={<Page1DetailA />} />
-          <Route path="detailB" element={<Page1DetailB />} />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
         </Route>
-        <Route path="/page2" element={<Page2 />} />
-      </Routes>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,12 +1,7 @@
-import { Link, Outlet } from "react-router-dom";
 export const Page1 = () => {
     return (
         <div>
             <h1>Page1ページです</h1>
-            <Link to="detailA">DetailAページ</Link>
-            <br />
-            <Link to="detailB">DetailBページ</Link>
-            <Outlet />
         </div>
     );
 }
