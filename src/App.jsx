7edaa3002ca80386f056e805e854b7054f34eeb0
rintarotajima <rintarotajima@gmail.com>
2024-10-10 14:53:31 +0900

    [update] ネストされたページ遷移

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Page1 } from "./Page1";
 import { Page1DetailA } from "./Page1DetailA";
 import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
-import { BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -18,9 +18,22 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: {url}}) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
