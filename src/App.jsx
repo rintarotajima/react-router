66cb25820dc7adce19a64edff1cfb0e29fb6a730
rintarotajima <rintarotajima@gmail.com>
2024-10-10 15:30:14 +0900

    [add] ルート定義の分割

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Page1DetailA } from "./Page1DetailA";
 import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-
+import { Router } from "./router/Router";
 export default function App() {
   return (
     <BrowserRouter>
@@ -13,31 +13,7 @@ export default function App() {
       <Link to="/page1">Page1</Link>
       <br />
       <Link to="/page2">Page2</Link>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: {url}}) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page2 } from "../Page2";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { page1routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
